d3d9f623808eda9fabd958ba183e79da4d6feb02
Rajan Maharjan <>
2020-03-17 14:33:07 +0545

    Endpoint URLs and HTTP Status Codes, Extensibility and Versioning

---
+++ api/controllers/contactsV1.js
@@ -0,0 +1,144 @@
+import { ObjectID } from "bson";
+import { errorHandler, generateFakeContacts } from "../utils";
+import { Contact } from "../models";
+
+const getContacts = async (req, res) => {
+
+    const contacts = await Contact.find({});
+
+    res.format({
+        json: () => res.json(contacts),
+        text: () => {
+            const contactAsText = [...contacts.values()]
+                .map(contact => Object.entries(contact).map(t => t.join(":")))
+                .join("\n\n ==========================>>    ");
+            res.send(contactAsText);
+        },
+
+        html: () => {
+            const html = [
+                `<table style="border: 1px solid black;">`,
+                `<th style="border: 1px solid black; background:red;">Contact ID</th>
+            <th style="border: 1px solid black; background:black; color:white;">Contact Data</th>
+            `
+            ];
+
+            contacts.forEach(({ _doc }) => {
+                let { _id, ...contact } = _doc;
+
+                // reorder properties
+                contact = Object.keys(contact)
+                    .sort()
+                    .reduce(
+                        (acc, key) => ({
+                            ...acc,
+                            [key]: _doc[key]
+                        }),
+                        {
+                            firstName: _doc.firstName,
+                            lastName: _doc.lastName
+                        }
+                    );
+                // console.log("getContacts -> contacts", contacts)
+                html.push(`
+                <tr style="border: 1px solid black;">
+                <td style="border: 1px solid black; background:yellow;">${_id}</td>
+                <td style="border: 1px solid black;">${Object.entries(contact)
+                        .map(([key, value]) => {
+                            return `<div style="border: solid green 5px;"><p><b>${key}</b>: ${JSON.stringify(
+                                value,
+                                function replacer(k, v) {
+                                    return k === "_id" ? undefined : v;
+                                },
+                            ).replace(/"/g, "")}</p></div>`;
+                        })
+                        .join("\n")}</td>
+                </tr>
+              `);
+            });
+
+            res.send(html.join("\n"));
+        }
+    });
+};
+
+const getContact = async (req, res, next) => {
+
+    const contactId = req.params.id;
+    contactId || next(errorHandler("Please enter a contact ID", 400));
+
+    contactId.length >= 24 || next(errorHandler("Invalid contact ID", 422));
+
+    const contact = await Contact.findOne({
+        _id: new ObjectID(contactId)
+    });
+    res.json(contact);
+};
+
+const postContact = async (req, res, next) => {
+
+    const contact = req.body;
+    contact || next(errorHandler("Please submit valid contact", 400));
+    (contact.primaryContactNumber) ||
+        next(errorHandler("Please submit valid contact", 422));
+    const newContact = new Contact({ ...contact });
+    const { _id, _doc } = await newContact.save();
+    _doc && _doc.primaryContactNumber
+        ? res
+            .status(201)
+            .set("location", `/contacts/${_id}`)
+            .json({ message: "Contact created", data: _doc })
+        : next(errorHandler("No contact created"));
+
+};
+
+const putContact = async (req, res, next) => {
+
+    const contactId = req.params.id;
+    const contactUpdate = req.body;
+    contactId || next(errorHandler("Please enter a contact ID", 400));
+    contactUpdate || next(errorHandler("Please submit valid contact", 400));
+    (contactUpdate && contactUpdate.primaryContactNumber) || next(errorHandler("Please submit valid contact", 422));
+    const result = await Contact.updateOne(
+        { _id: new ObjectID(contactId) },
+        { $set: contactUpdate }
+    );
+    result.nModified === 1
+        ? res.json({ message: "Contact updated" })
+        : next(errorHandler("No data updated"));
+};
+
+const deleteContact = async (req, res, next) => {
+
+    const contactId = req.params.id;
+    contactId || next(errorHandler("Please enter a contact ID", 422));
+
+    const result = await Contact.deleteOne({
+        _id: new ObjectID(contactId)
+    });
+
+    result.deletedCount === 1
+        ? res.json({ message: "Contact deleted" })
+        : next(errorHandler("No data deleted"));
+
+};
+
+const deleteAllContact = async (req, res, next) => {
+    const result = await Contact.deleteMany({});
+
+    result.deletedCount > 0
+        ? res.json({ message: "All contacts deleted" })
+        : next(errorHandler("No data deleted"));
+};
+
+const postManyContacts = async (req, res, next) => {
+    const n = parseInt(req.query.n);
+    n < 100 || next(errorHandler("Please enter number less than 100", 422));
+    const generatedContacts = await Contact.insertMany(generateFakeContacts(n));
+    res.status(201).json({
+        message: `${n} contacts generated`,
+        locations: generatedContacts.map(({ _id }) => `/api/v1/contacts/${_id}`)
+    });
+};
+
+export { getContacts, getContact, postContact, postManyContacts, putContact, deleteContact, deleteAllContact };
--- api/server.js
+++ api/server.js
@@ -1,12 +1,12 @@
 import { ServerConfig } from "./config";
-import { contacts, contacts_v2, groups } from "./routes";
+import { contactsV1, groupsV1, redirectRouter, contactsV2 } from "./routes";
 
 async function main() {
   const PORT = process.env.PORT || 3000;
   const server = new ServerConfig({
     port: PORT,
     // middleware: [],
-    routers: [contacts, contacts_v2, groups]
+    routers: [redirectRouter, contactsV2, contactsV1, groupsV1]
   });
 
   server.listen();
